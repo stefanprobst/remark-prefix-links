c63533a436166baae8eede619501708c40a11436
Stefan Probst <stefanprobst@kolabnow.com>
2018-09-30 15:07:04 +0200

    First version

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # remark-prefix-links
+
+Remark plugin to prefix relative links.
+
+## Example
+
+```js
+const remark = require('remark');
+const prefixLinks = require('remark-prefix-links');
+
+const result = remark()
+  .use(prefixLinks, '/prefix')
+  .processSync('Look at [this example](/example).');
+
+console.log(result.contents);
+// Look at [this example](/prefix/example).
+```
---
+++ index.js
@@ -0,0 +1,29 @@
+var path = require('path');
+var visit = require('unist-util-visit');
+
+module.exports = attacher;
+
+function attacher(pathPrefix) {
+  if (!pathPrefix) return;
+
+  return transformer;
+
+  function transformer(tree, file) {
+    visit(tree, 'link', visitor);
+
+    function visitor(node) {
+      if (node.url && !node.url.startsWith('#') && !isUrl(node.url)) {
+        node.url = path.join(pathPrefix, node.url);
+      }
+    }
+
+    function isUrl(url) {
+      try {
+        new URL(url);
+        return true;
+      } catch {
+        return false;
+      }
+    }
+  }
+}
